722ea2f72fa651f70b661a7b5b5b5b7a4fd82712
Rajish <rgaller@gazeta.pl>
2011-06-02 17:14:10 +0200

    Added new files for the commit with page size in the Activities view.

---
+++ productivity/app/util/ControllerUtil.java
@@ -0,0 +1,18 @@
+package util;
+
+import play.mvc.*;
+
+public class ControllerUtil extends Controller {
+
+	public static int setRowCount(int rowCount) {
+		if(rowCount == 0) {
+			rowCount = Integer.parseInt(session.get("rowCount"));
+			System.out.println("Activities.setRowCount() got from session: " + rowCount);
+		} else {
+			session.remove("rowCount");
+			session.put("rowCount", new Integer(rowCount));
+		}
+		return rowCount;
+	}
+
+}
