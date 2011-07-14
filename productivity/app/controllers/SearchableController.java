ddb4bf3faae552d8fbfe609f4a1e05633c868b8e
Rajish <rgaller@gazeta.pl>
2011-07-14 12:27:41 +0200

    Log4play instead of S.o.println

--- productivity/app/controllers/SearchableController.java
+++ productivity/app/controllers/SearchableController.java
@@ -1,9 +1,10 @@
 package controllers;
 
+import play.Logger;
+import play.mvc.After;
 import play.mvc.Before;
 import play.mvc.Controller;
 import util.Config;
-import controllers.ElasticSearchController;
 
 
 public class SearchableController extends Controller {
@@ -11,7 +12,7 @@ public class SearchableController extends Controller {
 	public static int setRowCount(int rowCount) {
 		if (rowCount == 0) {
 			rowCount = Integer.parseInt(session.get("rowCount"));
-			System.out.println("SearchableController.setRowCount() got from session: "
+			Logger.debug("SearchableController.setRowCount() got from session: "
 					+ rowCount);
 		} else {
 			session.remove("rowCount");
@@ -22,14 +23,18 @@ public class SearchableController extends Controller {
 
 	@Before
 	public static void setupRowCount() {
-		System.out.println("ControllerUtil.setupRowCount() session: " + session.all());
+		Logger.debug("SearchableController.setupRowCount() session: " + session.all());
 		if (!session.contains("rowCount")) {
-			System.out
-					.println("ControllerUtil.setupRowCount() no 'rowCount' in session. Setting default to "
+			Logger.info("ControllerUtil.setupRowCount() no 'rowCount' in session. Setting default to "
 							+ Config.ROW_COUNT);
 			session.put("rowCount", new Integer(Config.ROW_COUNT));
 		}
 
+	}
+
+	@After
+	public static void cleanUp() {
+		Logger.setUp("WARN");
 	}
 
 }
