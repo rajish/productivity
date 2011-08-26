6ee9022f718c68385fb8962f65404a8552d98ce9
Rajish <rgaller@gazeta.pl>
2011-08-27 00:55:41 +0200

    Made the timeline to show up for every div with class="timeline-main-div".

--- productivity/app/controllers/Application.java
+++ productivity/app/controllers/Application.java
@@ -1,15 +1,31 @@
 package controllers;
 
+import java.util.List;
+
 import models.Activity;
+import models.Task;
+import models.User;
+
+import controllers.deadbolt.Deadbolt;
+
 import play.mvc.Controller;
 import play.mvc.With;
-import controllers.deadbolt.Deadbolt;
+import play.mvc.Before;
 
 @With(Deadbolt.class)
 public class Application extends Controller {
 
+    @Before
+    public static void fillVars() {
+        // Logger.setUp("DEBUG");
+        List<Task> tasks = Task.findByUser("byIsActive", new Boolean(true));
+        List<User> users = User.findAll();
+        renderArgs.put("tasks", tasks);
+        renderArgs.put("users", users);
+    }
+
     public static void index() {
-    	long unassigned = Activity.count("task_id = null");
+        long unassigned = Activity.count("task_id = null");
         render(unassigned);
     }
 }
--- productivity/app/views/Application/_timeline.js
+++ productivity/app/views/Application/_timeline.js
@@ -1,36 +1,41 @@
 $(function () {
-	var tl;
-	var bandInfos = [
-		 Timeline.createBandInfo({
-			 width:          "70%", 
-			 intervalUnit:   Timeline.DateTime.MONTH, 
-			 intervalPixels: 100
-		 }),
-		 Timeline.createBandInfo({
-			 width:          "30%", 
-			 intervalUnit:   Timeline.DateTime.YEAR, 
-			 intervalPixels: 200
-		 })
-     ];
-	bandInfos[1].syncWith = 0;
-	bandInfos[1].highlight = true;
-	tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
+    var tl;
+    var bandInfos = [
+        Timeline.createBandInfo({
+            width:          "70%",
+            intervalUnit:   Timeline.DateTime.MONTH,
+            intervalPixels: 100
+        }),
+        Timeline.createBandInfo({
+            width:          "30%",
+            intervalUnit:   Timeline.DateTime.YEAR,
+            intervalPixels: 200
+        })
+    ];
+    bandInfos[1].syncWith = 0;
+    bandInfos[1].highlight = true;
+    tl = $(".timeline-main-div").map(function () {
+        return Timeline.create(this, bandInfos);
+    });
 
-	var resizeTimerID = null;
-	$('body').resize(fuction() {
-		if (resizeTimerID == null) {
-			resizeTimerID = window.setTimeout(function() {
-				resizeTimerID = null;
-				tl.layout();
-			}, 500);
-		}
-	});
+    var resizeTimerID = null;
+    $('body').resize(function() {
+        if (resizeTimerID == null) {
+            resizeTimerID = window.setTimeout(function() {
+                resizeTimerID = null;
+                tl.map(function() {
+                    this.layout();
+                });
+            }, 500);
+        }
+    });
 
-	tl.getBand(0).addOnScrollListener(function(band) {
-		var minDate = band.getMinDate();
-		var maxDate = band.getMaxDate();
-		/*if (true) {
-			eventSource.clear();
-			tl.loadJSON(...);
-		}*/
-	});
+    // tl.getBand(0).addOnScrollListener(function(band) {
+    //     var minDate = band.getMinDate();
+    //     var maxDate = band.getMaxDate();
+    //     if (true) {
+    //       eventSource.clear();
+    //       tl.loadJSON(...);
+    //       }
+    // });
+});
