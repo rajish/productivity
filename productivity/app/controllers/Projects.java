118f861f015c7630746f752cfc5bd370ab30bf0f
Rajish <rgaller@gazeta.pl>
2011-09-09 16:17:31 +0200

    Projects summary table works. Some data formatting may be needed.

--- productivity/app/controllers/Projects.java
+++ productivity/app/controllers/Projects.java
@@ -1,15 +1,28 @@
 package controllers;
 
+import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.List;
-import java.util.ArrayList;
+
+import javax.persistence.TypedQuery;
+
+import models.Activity;
 import models.Project;
-import play.mvc.Before;
-import play.mvc.Controller;
-import play.mvc.Util;
-import play.i18n.Messages;
-import play.data.validation.Validation;
+
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormat;
+import org.joda.time.format.PeriodFormatter;
+
 import play.data.validation.Valid;
+import play.db.jpa.JPA;
+import play.i18n.Messages;
+import play.mvc.Before;
+import play.mvc.Util;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 
 public class Projects extends SearchableController {
@@ -76,26 +89,37 @@ public class Projects extends SearchableController {
 
     public static void getStatistics() {
         List<Project> projects = Project.find("byIsActive", new Boolean(true)).fetch();
-        List<ProjectStatsModel> allStats = new ArrayList<ProjectStatsModel>();
-        for(Project proj : projects) {
-            ProjectStatsModel stats = new ProjectStatsModel(proj);
-            allStats.add(stats);
-        }
-        renderJSON(allStats);
+        renderJSON(projects, new ProjectSerializer());
     }
 
-    public static class ProjectStatsModel
-    {
-        public static String projName;
-        public static Date projStart;
-        public static Date projEnd;
-        public static Date realStart;
-        public static Date daysToDeadline;
-        public static Date workHours;
-        public ProjectStatsModel(Project proj) {
-            projName = proj.name;
-            projStart = proj.plannedStart;
-            projEnd = proj.plannedEnd;
+    public static class ProjectSerializer implements JsonSerializer<Project> {
+
+        @Override
+        public JsonElement serialize(Project proj, Type projType, JsonSerializationContext ctx) {
+            JsonObject retval = new JsonObject();
+            retval.addProperty("projName", proj.name);
+            retval.addProperty("projStart", proj.plannedStart.toString());
+            retval.addProperty("projEnd", proj.plannedEnd.toString());
+
+            TypedQuery<Date> q1 = JPA.em().createQuery(
+                    "select min(act.timestamp) from Activity as act, Task as task where act.task = task and task.project = :proj", Date.class);
+            q1.setParameter("proj", proj);
+            Date d = q1.getSingleResult();
+            retval.addProperty("realStart", d.toString());
+
+            Period deadline = new Period(proj.plannedEnd.getTime() - (new Date()).getTime());
+            PeriodFormatter pf = PeriodFormat.getDefault();
+            retval.addProperty("daysToDeadline", deadline.toString(pf));
+
+            List<Activity> acts = JPA.em().createQuery(
+                    "select act from Activity as act, Task as task where act.task = task and task.project = :proj",
+                    Activity.class).setParameter("proj", proj).getResultList();
+            Period wh = new Period();
+            for(Activity act : acts) {
+                wh.plus(act.duration());
+            }
+            retval.addProperty("workHours", wh.toString(pf));
+            return retval;
         }
     }
 
