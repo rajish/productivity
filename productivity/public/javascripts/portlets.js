08f37bb232d2f4e27b34c3da2cf1ea79cac36a5a
Rajish <rgaller@gazeta.pl>
2011-09-12 17:24:30 +0200

    WIP Portlets architecture: Improved multi-column layout. Started with serialization.

--- productivity/public/javascripts/portlets.js
+++ productivity/public/javascripts/portlets.js
@@ -2,12 +2,14 @@
 Number.prototype.pad = function(ndigits) {
     var n = this;
     var p = isNaN(ndigits) ? 2 : ndigits;
-    var zeros = $.map(new Array(p - 1), function () { return "0" }).join("");
+    var zeros = $.map(new Array(p - 1), function () { return "0"; }).join("");
     if( p > 0)
         return (zeros + n).substring(n.toString().length - p + 1);
     else
         return n;
 };
+
+var portletConfig = null;
 
 $(function () {
     //
@@ -15,18 +17,24 @@ $(function () {
     //
     $( ".column" ).sortable({
         connectWith: ".column",
-        axis: "y",
+        forcePlaceholderSize: true,
+        opacity: 0.5,
+        //axis: "y",
+        start: function(event, ui) {
+            $(".ui-sortable-placeholder").css("height", ui.item.css("height"));
+        },
         update: function(event, ui) {
             // TODO: (re)store user layout changes
+            portletConfig = $( ".column" ).sortable("serialize");
         }
     });
 
     $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-            .find( ".portlet-header" )
+        .find( ".portlet-header" )
             .addClass( "ui-widget-header ui-corner-all" )
             .prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
             .end()
-            .find( ".portlet-content" );
+        .find( ".portlet-content" );
 
     $( ".portlet-header .ui-icon" ).click(function() {
         $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
